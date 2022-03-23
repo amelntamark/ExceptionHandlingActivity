ea308113089169e8c8c67c5090c0b36c25700e14
Amel <amelntamark@gmail.com>
2022-03-23 12:03:39 -0700

    function successfully raises KeyError

---
+++ activity/main.py
@@ -0,0 +1,9 @@
+def key_lookup(key, dict):
+    return dict[key]
+
+# bag = {
+#         "glasses": 1,
+#         "pens": 2,
+#         "wallet": 1
+#         }
+# key_lookup("key", bag)
--- tests/test_exception.py
+++ tests/test_exception.py
@@ -3,4 +3,11 @@ import pytest
 from activity.main import *
 
 def test_exception():
-    assert True
+    bag = {
+        "glasses": 1,
+        "pens": 2,
+        "wallet": 1
+        }
+    
+    with pytest.raises(KeyError):
+        key_lookup("key", bag)
